afb5c870a3a6faa6a91981b307259e2617d3983c
Andrew Spott <andrew.spott@gmail.com>
2014-07-16 11:54:34 -0600

    matrix multiplication appears to work for diagonal matrices.

--- src/Data/Sparse.hs
+++ src/Data/Sparse.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
 module Data.Sparse where
@@ -7,37 +8,55 @@ import qualified Data.Vector.Unboxed as U
 type Index = Integer
 
 
-class (Num val) => Vector v val where
-        (<!!>) :: v val -> Int -> val
+{-class (Num val) => Vector v val where-}
+        {-(!) :: v val -> Int -> val-}
 
 class (Num val) => Matrix m val where
         -- multiply a matrix by a vector
-        mul :: (Vector v val) => m val  -> v val -> v val
+        (<**>) :: m val -> U.Vector val -> U.Vector val
         -- update the diagonal
         updateDiagonal :: m val -> (Int -> val) -> m val
         -- get a row:
-        (<!>) :: m val -> Int -> U.Vector val
+        (!!) :: m val -> Int -> U.Vector val
 
-data CSparseRow val = CSparseRow     { values     :: U.Vector val
-                                     , colIndexes :: U.Vector Int
+data CSparseRow val = CSparseRow     { values     :: !(U.Vector val)
+                                     , colIndexes :: !(U.Vector Int)
                                      --the start index in values and
                                      --colIndices of each row.  should be
                                      --terminated with "rows"
-                                     , rowIndexes :: U.Vector Int
-                                     , rows       :: Int
-                                     , cols       :: Int }
+                                     , rowIndexes :: !(U.Vector Int)
+                                     , rows       :: !Int
+                                     , cols       :: !Int }
                                      deriving Show
 
-csparseRowGetRow :: (Num val) => CSparseRow val -> Int -> U.Vector val
+{-diagonalMatrix :: (U.Unbox val, Num val) => (Int, Int) -> (Int -> val) -> CSparseRow val-}
+{-diagonalMatrix size f = -}
+
+csparseRowGetRow :: (U.Unbox val, Num val) => CSparseRow val -> Int -> U.Vector val
 csparseRowGetRow m row = let vec = U.replicate c 0 in
-                             U.update_ vec (part (colIndexes m)) (part (values m))
-        where part = U.slice (rowIndexes m U.! row) (rowIndexes m U.! (row + 1))
-              r = rows m
+                             U.update_ vec partcolIndexes partValues
+        where partValues = U.slice (rowIndexes m U.! row) (rowIndexes m U.! (row + 1)) (values m)
+              partcolIndexes = U.slice (rowIndexes m U.! row) (rowIndexes m U.! (row + 1)) (colIndexes m)
               c = cols m
 
-{-CSparseRowMul :: (Vector v ord val) => CSparseRow ord val -> v ord val -> v ord val-}
-{-CSparseRowMul m v = -}
+csparseRowMul :: (U.Unbox val, Num val) => CSparseRow val -> U.Vector val -> U.Vector val
+csparseRowMul m v = U.generate r rowCalc
+        where rowCalc row = U.sum $ U.ifilter (\ i _ -> i `U.elem` partcolIndexes row) $ U.accumulate_ (*) v (partcolIndexes row) (partValues row)
+              partValues row = U.slice (slice row) (slice (row + 1) - slice row) (values m)
+              partcolIndexes row = U.slice (slice row) (slice (row + 1) - slice row) (colIndexes m)
+              slice row = U.snoc (rowIndexes m) r U.! row
+              r = rows m
 
---instance Matrix m ord val => Sparse m ord val where
+csparseUpdateDiagonal :: (Num val) => CSparseRow val -> (Int -> val) -> CSparseRow val
+csparseUpdateDiagonal m f = m
+
+instance (U.Unbox val, Num val) => Matrix CSparseRow val where
+        (<**>) = csparseRowMul
+        (!!) = csparseRowGetRow
+        updateDiagonal = csparseUpdateDiagonal
+
+{-instance (Num val) => Vector U.Vector val where-}
+        {-(!) = (U.!)-}
 
 
+--
