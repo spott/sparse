0e3c8bdf8adcf9e989a5ac8194d8cb68592db378
Andrew <andrew.spott@gmail.com>
2014-07-17 00:10:45 +0000

    added "Index" type.  This is more useful than the generic (Int, Int) that it replaces.

--- src/Data/Sparse.hs
+++ src/Data/Sparse.hs
@@ -5,8 +5,7 @@ module Data.Sparse where
 
 import qualified Data.Vector.Unboxed as U
 
-type Index = Integer
-
+data Index = Index {row_ :: Int, col_ :: Int } deriving Show
 
 {-class (Num val) => Vector v val where-}
         {-(!) :: v val -> Int -> val-}
@@ -19,14 +18,15 @@ class (Num val) => Matrix m val where
         -- get a row:
         (!!) :: m val -> Int -> U.Vector val
 
+
+
 data CSparseRow val = CSparseRow     { values     :: !(U.Vector val)
                                      , colIndexes :: !(U.Vector Int)
                                      --the start index in values and
                                      --colIndices of each row.  should be
                                      --terminated with "rows"
                                      , rowIndexes :: !(U.Vector Int)
-                                     , rows       :: !Int
-                                     , cols       :: !Int }
+                                     , size :: !Index}
                                      deriving Show
 
 {-diagonalMatrix :: (U.Unbox val, Num val) => (Int, Int) -> (Int -> val) -> CSparseRow val-}
@@ -37,7 +37,7 @@ csparseRowGetRow m row = let vec = U.replicate c 0 in
                              U.update_ vec partcolIndexes partValues
         where partValues = U.slice (rowIndexes m U.! row) (rowIndexes m U.! (row + 1)) (values m)
               partcolIndexes = U.slice (rowIndexes m U.! row) (rowIndexes m U.! (row + 1)) (colIndexes m)
-              c = cols m
+              c = col_ $ size m
 
 csparseRowMul :: (U.Unbox val, Num val) => CSparseRow val -> U.Vector val -> U.Vector val
 csparseRowMul m v = U.generate r rowCalc
@@ -45,7 +45,7 @@ csparseRowMul m v = U.generate r rowCalc
               partValues row = U.slice (slice row) (slice (row + 1) - slice row) (values m)
               partcolIndexes row = U.slice (slice row) (slice (row + 1) - slice row) (colIndexes m)
               slice row = U.snoc (rowIndexes m) r U.! row
-              r = rows m
+              r = row_ $ size m
 
 csparseUpdateDiagonal :: (Num val) => CSparseRow val -> (Int -> val) -> CSparseRow val
 csparseUpdateDiagonal m f = m
